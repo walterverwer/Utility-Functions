b3189ff95a9dac92999d957d1a9c105d8deccc69
Walter Verwer <walterverwer@gmail.com>
2021-05-05 18:46:00 +0200

    r_importer + hard dependencies

--- walpy/__init__.py
+++ walpy/__init__.py
@@ -3,6 +3,24 @@
 @git: https://github.com/walterverwer
 
 """
+
+# Let users know if they're missing any of our hard dependencies
+hard_dependencies = ('rpy2')
+missing_dependencies = []
+
+for dependency in hard_dependencies:
+    try:
+        __import__(dependency)
+    except ImportError as e:
+        missing_dependencies.append(f'{dependency}: {e}')
+
+if missing_dependencies:
+    raise ImportError(
+        'Unable to import required dependencies:\n' + '\n'.join(missing_dependencies)
+    )
+del hard_dependencies, dependency, missing_dependencies
+
+# Functions in walpy
 from .r_importer import r_importer
 from .suppress import suppress
 from .write_run import write_run
